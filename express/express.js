a2bf0bf7ef0381ff8d1c419569d5ab7cbccc6b82
DMisilin <Milissa990@yandex.ru>
2019-05-29 14:36:38 +0300

    e

--- express/express.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const server = express();
-
-server.get('/', (req, res) => {
-    res.sendfile('/home/dmisilin/NodeJS/Chat_2.0/page/front.html');
-})
-server.listen(3003, () => {
-    console.log('connected...listning 3003 port!');
-})
